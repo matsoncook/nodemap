1dfadf1b1a1051666f2859ae5c68418dc141fc42
matsoncook <markcook2001@gmail.com>
2025-08-03 01:32:55 +0000

    replit stuff

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 // Replit uses a system-assigned port
 const PORT = process.env.PORT || 3000;
 
 // Use __dirname to resolve relative paths
-const mapDir = path.join(__dirname, 'web', 'map');
-app.use('/map', express.static(mapDir));
+const mapDir = path.join(__dirname, "web", "map");
+app.use("/map", express.static(mapDir));
 
-const mapStaticDir = path.join(__dirname, 'web', 'map', 'static');
-app.use('/map_static', express.static(mapStaticDir));
+const mapStaticDir = path.join(__dirname, "web", "map", "static");
+app.use("/map_static", express.static(mapStaticDir));
 
-const mapImage = path.join(__dirname, 'mapimages', 'satellite');
-app.use('/mapimage', express.static(mapImage));
+const mapImage = path.join(__dirname, "mapimages", "satellite");
+app.use("/mapimage", express.static(mapImage));
 
 // Serve the root map folder at /
-app.use('/', express.static(mapDir));
+rootDir = path.join(__dirname);
+app.use("/", express.static(rootDir));
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
