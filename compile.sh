4f16854de7938d58dba174aa5931f69743c7b0fc
Mark Cook <markcook2001@gmail.com>
2025-08-03 21:41:10 +0000

    compile

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
- export PATH=${PATH}:/home/mark/node/node-v20.19.4-linux-x64/bin
-
+#export PATH=${PATH}:/home/mark/node/node-v20.19.4-linux-x64/bin
+export PATH=${PATH}:/home/ubuntu/node/node-v23.10.0-linux-x64/bin
  npx tsc
  npx webpack
