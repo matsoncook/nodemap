28e9b1ff4ca22b08787d9143dc4146d4e3903026
matsoncook <markcook2001@gmail.com>
2025-08-09 23:10:54 +0000

    control

--- src/control/Control.ts
+++ src/control/Control.ts
@@ -19,33 +19,52 @@ export default class Control
   setup(){
         
         if (this.btn && this.input && this.result && this.redrawMapGrid && this.viewportCanvasMagnification && this.canvasPositionOfViewportCenterX && this.canvasPositionOfViewportCenterY) {
-          this.result.textContent = `Hello, ${this.viewport.toString()}!`;
-          this.viewportCanvasMagnification.valueAsNumber = this.viewport.viewPortCanvasMagnification.x;
-          this.canvasPositionOfViewportCenterX.valueAsNumber = this.viewport.canvasPositionOfViewportCenter.x;
-          this.canvasPositionOfViewportCenterY.valueAsNumber = this.viewport.canvasPositionOfViewportCenter.y;
+          this.populateInputs();
 
-          this.btn.addEventListener("click", () => {
-            if (this.btn && this.input && this.result && this.redrawMapGrid && this.viewportCanvasMagnification && this.canvasPositionOfViewportCenterX && this.canvasPositionOfViewportCenterY) {
-              this.viewport.viewPortCanvasMagnification.set2(this.viewportCanvasMagnification.valueAsNumber,this.viewportCanvasMagnification.valueAsNumber);
-              this.viewport.canvasPositionOfViewportCenter.set2(this.canvasPositionOfViewportCenterX.valueAsNumber,this.canvasPositionOfViewportCenterY.valueAsNumber);
-
-
-              this.map.draw();
-                if(this.redrawMapGrid.value.trim() === "1")
-                {
-                  this.map.mapGrid.load1(this.mercatorViewport.zoom, (image) => {
-                    this.map.draw();
-                    });
-                }
-                var value = this.input.textContent.trim();
-                if (!value) {
-                  this.result.textContent = "Please enter a name.";
-                    value = "World";
-                }
-            this.result.textContent = `Hello, ${this.viewport.toString()}!`;
-
-            }});
+          this.btn.addEventListener("click", this.buttonEvent);
         }
     }
+
+    buttonEvent()
+  {
+    if (this.btn && this.input && this.result && this.redrawMapGrid && this.viewportCanvasMagnification && this.canvasPositionOfViewportCenterX && this.canvasPositionOfViewportCenterY) {
+        
+      this.populateMapParameters();
+
+      this.map.draw();
+        if(this.redrawMapGrid.value.trim() === "1")
+        {
+          this.map.mapGrid.load1(this.mercatorViewport.zoom, (image) => {
+            this.map.draw();
+            });
+        }
+        var value = this.input.textContent.trim();
+        if (!value) {
+          this.result.textContent = "Please enter a name.";
+            value = "World";
+        }
+    this.result.textContent = `Hello, ${this.viewport.toString()}!`;
+  }
+  }
+
+  populateInputs()
+  {
+      if (this.btn && this.input && this.result && this.redrawMapGrid && this.viewportCanvasMagnification && this.canvasPositionOfViewportCenterX && this.canvasPositionOfViewportCenterY)
+      {
+        this.result.textContent = `Hello, ${this.viewport.toString()}!`;
+        this.viewportCanvasMagnification.valueAsNumber = this.viewport.viewPortCanvasMagnification.x;
+        this.canvasPositionOfViewportCenterX.valueAsNumber = this.viewport.canvasPositionOfViewportCenter.x;
+        this.canvasPositionOfViewportCenterY.valueAsNumber = this.viewport.canvasPositionOfViewportCenter.y;
+      }
+  }
+
+  populateMapParameters()
+  {
+      if (this.btn && this.input && this.result && this.redrawMapGrid && this.viewportCanvasMagnification && this.canvasPositionOfViewportCenterX && this.canvasPositionOfViewportCenterY)
+      {
+        this.viewport.viewPortCanvasMagnification.set2(this.viewportCanvasMagnification.valueAsNumber,this.viewportCanvasMagnification.valueAsNumber);
+        this.viewport.canvasPositionOfViewportCenter.set2(this.canvasPositionOfViewportCenterX.valueAsNumber,this.canvasPositionOfViewportCenterY.valueAsNumber);
+      }
+  }
   
 }
